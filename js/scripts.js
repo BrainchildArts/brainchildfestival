c5512ed1f377deaa03d20f3b1bc96fee104c7737
Jamie Davies <viralpickaxe@gmail.com>
2014-12-19 11:01:04 +0000

    Some more stuff

--- js/scripts.js
+++ js/scripts.js
@@ -20009,15 +20009,25 @@ $(window).load(function() {
 
 // Overlay
 
-$(".tile").click(function(e) {
-    var tile = $(this),
-        name = tile.data("title"),
-        img = tile.data("img"),
-        desc = tile.data("desc"),
-        link = tile.data("link");
+// $(".tile").click(function(e) {
+//     var tile = $(this),
+//         name = tile.data("title"),
+//         img = tile.data("img"),
+//         desc = tile.data("desc"),
+//         link = tile.data("link");
+//     e.preventDefault();
+//     $("html").addClass("hide-overflow");
+//     $("body").append("<div class='overlay'><a href='#' class='overlay__close'>Close</a><div class='col-1-2'><img class='overlay__image' src='" + img + "' alt='" + name + "' /></div><div class='col-1-2'><div class='overlay__copy'><h4>" + name + "</h4><p>" + desc + "</p></div></div></div>");
+// });
+
+$('.tile-list').click(function(e){
     e.preventDefault();
-    $("html").addClass("hide-overflow");
-    $("body").append("<div class='overlay'><a href='#' class='overlay__close'>Close</a><div class='col-1-2'><img class='overlay__image' src='" + img + "' alt='" + name + "' /></div><div class='col-1-2'><div class='overlay__copy'><h4>" + name + "</h4><p>" + desc + "</p></div></div></div>");
+    var $this = $(this);
+    var image = $this.data('image');
+    var title = $this.data('title');
+    var description = $this.data('description');
+    console.log(title);
+    return false;
 });
 
 $("body").on("click", ".overlay__close", function(e) {
