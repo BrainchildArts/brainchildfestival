dba09c808da6493c7cd7ad64e26f42b33c093803
Jerome Toole <jerometoole@gmail.com>
2014-12-30 18:04:43 +0000

    add email form // remove instagram // add andrew attrib // improve overlay // header larger

--- js/scripts.js
+++ js/scripts.js
@@ -20012,7 +20012,7 @@ $(".tile").click(function(e) {
         link = tile.data("link");
     e.preventDefault();
     $("html").addClass("hide-overflow");
-    $("body").append("<div class='overlay'><a href='#' class='overlay__close'>Close</a><div class='col-1-2'><img class='overlay__image' src='" + img + "' alt='" + name + "' /></div><div class='col-1-2'><div class='overlay__copy'><h4>" + name + "</h4><p>" + desc + "</p></div></div></div>");
+    $("body").append("<div class='overlay'><a href='#' class='overlay__close'>Close</a><div class='col-1-2'><div class='overlay__copy'><h4>" + name + "</h4><p>" + desc + "</p></div></div><div class='col-1-2'><img class='overlay__image' src='" + img + "' alt='" + name + "' /></div></div>");
 });
 
 $("body").on("click", ".overlay__close", function(e) {
