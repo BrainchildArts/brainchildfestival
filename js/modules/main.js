b9cbc461d76b5a648efe80b709af2bdc24b848f1
Jerome Toole <jerometoole@gmail.com>
2015-03-12 21:59:26 +0000

    youtube in overlay

--- js/modules/main.js
+++ js/modules/main.js
@@ -121,15 +121,19 @@ $("#lineup .tile").click(function(e) {
         img = tile.data("img"),
         desc = tile.data("desc"),
         link = tile.data("link"),
+        utube = tile.data("utube-id"),
         soundcloud = tile.data("sc");
     e.preventDefault();
     $("html").addClass("hide-overflow");
-    $("body").append("<div class='overlay-wrap'><a href='#' class='overlay__close'>Close</a><div class='overlay'><div class='col-1-2'><div class='overlay__copy'><h4>" + name + "</h4><p>" + desc + "</p></div></div><div class='col-1-2'><img class='overlay__image' src='" + img + "' alt='" + name + "' /></div></div></div>");
+    $("body").append("<div class='overlay-wrap'><a href='#' class='overlay__close'>Close</a><div class='overlay'><div class='col-1-2'><div class='overlay__copy'><h4>" + name + "</h4><p>" + desc + "</p></div><div class='overlay__embeds'></div></div><div class='col-1-2'><img class='overlay__image' src='" + img + "' alt='" + name + "' /></div></div></div>");
     if ($( this ).data("link")) {
-      $(".overlay__copy").append("<p><a target='_blank' class='overlay__link' href='" + link + "'>" + name + "</a></p>");
+      $(".overlay__copy").append("<p> See more <a target='_blank' class='overlay__link' href='" + link + "'>here</a>.</p>");
     }
     if ($( this ).data("sc")) {
-      $(".overlay__copy").append("<p><iframe id='sc-widget' src='https://w.soundcloud.com/player/?url=https://soundcloud.com/" + soundcloud + "' width='100%' height='150' scrolling='no' frameborder='no'></iframe></p>");
+      $(".overlay__embeds").append("<p><iframe id='sc-widget' src='https://w.soundcloud.com/player/?url=https://soundcloud.com/" + soundcloud + "' width='100%' height='110' scrolling='no' frameborder='no'></iframe></p>");
+    }
+    if ($( this ).data("utube-id")) {
+      $(".overlay__embeds").append("<p><div class='embed-container'><iframe src='http://www.youtube.com/embed/" + utube + "' frameborder='0' allowfullscreen></iframe></div></p>");
     }
 });
 
