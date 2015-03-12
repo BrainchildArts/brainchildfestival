f1a87cb8431ec0b30db131aeef7d6c4993026d59
Jerome Toole <jerometoole@gmail.com>
2015-03-12 19:21:58 +0000

    conditional bits

--- dist/scripts.js
+++ dist/scripts.js
@@ -19970,25 +19970,24 @@ $(document).ready(function() {
 $(document).ready(function() {
   // init Isotope
   var $container = $('.tiles-small');
-  // init
-  if ($(window).innerWidth() > 440) {
+  function isolineup() {
     $container.isotope({
       itemSelector: '.tile',
       masonry: {
-        isFitWidth: true
+        isFitWidth: true,
+        sortBy: 'tile__heading'
       }
     });
+  }
+  // init
+  if ($(window).innerWidth() > 440) {
+    isolineup();
   }
   $(window).resize(function () {
     if ($(window).innerWidth() < 440) {
         $container.isotope('destroy');
     } else {
-        $container.isotope({
-            itemSelector: '.tile',
-            masonry: {
-            isFitWidth: true
-          }
-        });
+        isolineup();
     }
   });
   // bind filter button click
@@ -20074,6 +20073,7 @@ $("#friends .tile").click(function(e) {
     $("body").append("<div class='overlay-wrap'><a href='#' class='overlay__close'>Close</a><div class='overlay'><div class='col-1-2'><div class='overlay__copy'><h4>" + name + "</h4><p>" + desc + "</p><p><a target='_blank' class='overlay__link' href='" + link + "'>" + name + "</a></p></div></div><div class='col-1-2'><img class='overlay__image' src='" + img + "' alt='" + name + "' /></div></div></div>");
 });
 
+///////////lineup main bit.
 $("#lineup .tile").click(function(e) {
     var tile = $(this),
         name = tile.data("title"),
@@ -20083,8 +20083,15 @@ $("#lineup .tile").click(function(e) {
         soundcloud = tile.data("sc");
     e.preventDefault();
     $("html").addClass("hide-overflow");
-    $("body").append("<div class='overlay-wrap'><a href='#' class='overlay__close'>Close</a><div class='overlay'><div class='col-1-2'><div class='overlay__copy'><h4>" + name + "</h4><p>" + desc + "</p><p><a target='_blank' class='overlay__link' href='" + link + "'>" + name + "</a></p><p><iframe id='sc-widget' src='https://w.soundcloud.com/player/?url=https://soundcloud.com/" + soundcloud + "' width='100%' height='150' scrolling='no' frameborder='no'></iframe></p></div></div><div class='col-1-2'><img class='overlay__image' src='" + img + "' alt='" + name + "' /></div></div></div>");
+    $("body").append("<div class='overlay-wrap'><a href='#' class='overlay__close'>Close</a><div class='overlay'><div class='col-1-2'><div class='overlay__copy'><h4>" + name + "</h4><p>" + desc + "</p></div></div><div class='col-1-2'><img class='overlay__image' src='" + img + "' alt='" + name + "' /></div></div></div>");
+    if ($( this ).data("link")) {
+      $(".overlay__copy").append("<p><a target='_blank' class='overlay__link' href='" + link + "'>" + name + "</a></p>");
+    }
+    if ($( this ).data("sc")) {
+      $(".overlay__copy").append("<p><iframe id='sc-widget' src='https://w.soundcloud.com/player/?url=https://soundcloud.com/" + soundcloud + "' width='100%' height='150' scrolling='no' frameborder='no'></iframe></p>");
+    }
 });
+
 
 $(".info-overlay").click(function(e) {
     var tile = $(this),
