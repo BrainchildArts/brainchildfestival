6c26440c6fb38489bc837815a7e4cb7cdc98d749
Jerome Toole <jerometoole@gmail.com>
2015-03-13 10:45:46 +0000

    working on images

--- dist/scripts.js
+++ dist/scripts.js
@@ -20098,6 +20098,9 @@ $("#lineup .tile").click(function(e) {
     e.preventDefault();
     $("html").addClass("hide-overflow");
     $("body").append("<div class='overlay-wrap'><a href='#' class='overlay__close'>Close</a><div class='overlay'><div class='col-1-2'><div class='overlay__copy'><h4>" + name + "</h4><p>" + desc + "</p></div><div class='overlay__embeds'></div></div><div class='col-1-2'><img class='overlay__image' src='" + img + "' alt='" + name + "' /></div></div></div>");
+    if ($( this ).data("img")) {
+      $(".overlay__embeds").append("<p><iframe id='sc-widget' src='https://w.soundcloud.com/player/?url=https://soundcloud.com/" + soundcloud + "' width='100%' height='110' scrolling='no' frameborder='no'></iframe></p>");
+    }
     if ($( this ).data("link")) {
       $(".overlay__copy").append("<p> See more <a target='_blank' class='overlay__link' href='" + link + "'>here</a>.</p>");
     }
