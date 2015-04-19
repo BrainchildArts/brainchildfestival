53422e3453f74e93d43d03372a8e982f3d4758e8
Jerome Toole <jerometoole@gmail.com>
2015-04-19 19:03:12 +0100

    update

--- dist/scripts.js
+++ dist/scripts.js
@@ -19442,13 +19442,13 @@ jQuery.extend( jQuery.easing,
 (function() {
   var $;
   $ = jQuery;
-  (function($){var g,d,j=1,a,b=this,f=!1,h="postMessage",e="addEventListener",c,i=b[h]&&!$.browser.opera;$[h]=function(k,l,m){if(!l){return}k=typeof k==="string"?k:$.param(k);m=m||parent;if(i){m[h](k,l.replace(/([^:]+:\/\/[^\/]+).*/,"$1"))}else{if(l){m.location=l.replace(/#.*$/,"")+"#"+(+new Date)+(j++)+"&"+k}}};$.receiveMessage=c=function(l,m,k){if(i){if(l){a&&c();a=function(n){if((typeof m==="string"&&n.origin!==m)||($.isFunction(m)&&m(n.origin)===f)){return f}l(n)}}if(b[e]){b[l?e:"removeEventListener"]("message",a,f)}else{b[l?"attachEvent":"detachEvent"]("onmessage",a)}}else{g&&clearInterval(g);g=null;if(l){k=typeof m==="number"?m:typeof k==="number"?k:100;g=setInterval(function(){var o=document.location.hash,n=/^#?\d+&/;if(o!==d&&n.test(o)){d=o;l({data:o.replace(n,"")})}},k)}}}})(jQuery);;
+  (function($){var g,d,j=1,a,b=this,f=!1,h="postMessage",e="addEventListener",c,i=b[h];$[h]=function(k,l,m){if(!l){return}k=typeof k==="string"?k:$.param(k);m=m||parent;if(i){m[h](k,l.replace(/([^:]+:\/\/[^\/]+).*/,"$1"))}else{if(l){m.location=l.replace(/#.*$/,"")+"#"+(+new Date)+(j++)+"&"+k}}};$.receiveMessage=c=function(l,m,k){if(i){if(l){a&&c();a=function(n){if((typeof m==="string"&&n.origin!==m)||($.isFunction(m)&&m(n.origin)===f)){return f}l(n)}}if(b[e]){b[l?e:"removeEventListener"]("message",a,f)}else{b[l?"attachEvent":"detachEvent"]("onmessage",a)}}else{g&&clearInterval(g);g=null;if(l){k=typeof m==="number"?m:typeof k==="number"?k:100;g=setInterval(function(){var o=document.location.hash,n=/^#?\d+&/;if(o!==d&&n.test(o)){d=o;l({data:o.replace(n,"")})}},k)}}}})(jQuery);;
   $.fn.stratus = function(settings) {
     return $.stratus(settings);
   };
   $.stratus = function(settings) {
     var root_url, src;
-    root_url = settings.env === 'development' ? 'http://example.com:3000' : 'http://www.stratus.sc';
+    root_url = settings.env === 'development' ? 'http://example.com:3000' : 'http://stratus.sc';
     $('head').append("<link rel='stylesheet' href='" + root_url + "/stratus.css' type='text/css'/>");
     if (settings.align === 'top') {
       $('head').append("<style>#stratus{ top: 0; }</style>");
@@ -19470,7 +19470,7 @@ jQuery.extend( jQuery.easing,
       });
     });
     $('#stratus').show();
-    $('a.stratus').click(function() {
+    $('body').on("click", "a.stratus", function() {
       $.postMessage($(this).attr('href'), src, $('#stratus iframe')[0].contentWindow);
       return false;
     });
